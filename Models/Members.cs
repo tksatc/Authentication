6ea2d62fc2ba7918c4ff3ae23d13d12798d7d8b3
tksatc <tksatc@gmail.com>
2021-11-30 15:01:14 -0800

    Add project files.

---
+++ Models/Members.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Authentication.Models
+{
+    public class Members
+    {
+        public int ID { get; set; }
+        [Required]
+        [StringLength(30,ErrorMessage ="Please enter your first name using 30 characters or less.")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(30, ErrorMessage = "Please enter your last name using 30 characters or less.")]
+        [Display (Name ="Last Name")]
+        public string LastName { get; set; }
+        [StringLength(50, ErrorMessage = "Please enter your address using 50 characters or less.")]
+        public  string Address { get; set; }
+        [StringLength(30, ErrorMessage = "Please enter the city using 30 characters or less.")]
+        public string City { get; set; }
+        [StringLength(2, ErrorMessage = "Please enter the state using 2 characters.")]
+        public string State { get; set; }
+        [StringLength(10, ErrorMessage = "Zipcode has a maximum length of 10 numbers.")]
+        public string Zip { get; set; }
+        public string Email { get; set; }
+        [DataType(DataType.PhoneNumber)]
+        [Display(Name = "Cell#")]
+        public string MobilePhone { get; set; }
+
+        public ICollection<ParentChild> Children { get; set; }
+    }
+}
